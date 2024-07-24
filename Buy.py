1267cfae0135d371692174d57ee93844fc4a06d8
STSARC001 <121634358+STSARC001@users.noreply.github.com>
2024-07-24 09:57:21 -0700

    Update Buy.py

--- Buy.py
+++ Buy.py
@@ -79,5 +79,5 @@ if __name__ == '__main__':
     threading.Thread(target=main).start()
     
     # Start the Flask server
-    port = int(os.environ.get('PORT', 5000))  # Use PORT env variable or default to 5000
+    port = int(os.environ.get('PORT', 4000))  # Use PORT env variable or default to 5000
     app.run(host='0.0.0.0', port=port)
